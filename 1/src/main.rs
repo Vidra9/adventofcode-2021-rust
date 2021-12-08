7865352af0d84f299d55d8c97939e483fcf6e0cc
Mihailo Vidakovic <mihailo.system@gmail.com>
2021-12-08 17:45:55 +0100

    [sonar_sweep] Switched from loop to for

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,26 +1,22 @@
 use std::fs;
 
 fn main() {
-    println!("reading file");
-
     let mut result = 0;
 
     let file_contents = fs::read_to_string("input.txt")
         .expect("Something went wrong");
-    let mut lines = file_contents.lines();
-    let line : String = lines.next().unwrap().to_string();
-    let mut previous : u32 = line.trim().parse().expect("Line not a number");
-    loop {
-        let next_line = lines.next();
-        if next_line == Option::None {
-            break;
-        }
-        let line : String = next_line.unwrap().to_string();
-        let next : u32 = match line.trim().parse() {Ok(num) => num, Err(_) => break,};
 
-        if next > previous {
+    let lines = file_contents.lines();
+    let mut previous : i32 = -1;
+
+    for next_line in lines {
+        let line : String = next_line.to_string();
+        let next : i32 = match line.trim().parse() {Ok(num) => num, Err(_) => break,};
+
+        if previous > -1 && next > previous {
             result += 1;
         }
+
         previous = next;
     }
 
