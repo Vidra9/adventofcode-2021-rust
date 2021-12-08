7b3105cd7c7dfaf8c5a3c9b81e9197c48e9486cb
Mihailo Vidakovic <mihailo.system@gmail.com>
2021-12-08 16:52:56 +0100

    Added sweep sonar PART 1 solution

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -3,8 +3,26 @@ use std::fs;
 fn main() {
     println!("reading file");
 
-    let file = fs::read_to_string("input.txt")
+    let mut result = 0;
+
+    let file_contents = fs::read_to_string("input.txt")
         .expect("Something went wrong");
-    
-    println!("File contents: {}", file);
+    let mut lines = file_contents.lines();
+    let line : String = lines.next().unwrap().to_string();
+    let mut previous : u32 = line.trim().parse().expect("Line not a number");
+    loop {
+        let next_line = lines.next();
+        if next_line == Option::None {
+            break;
+        }
+        let line : String = next_line.unwrap().to_string();
+        let next : u32 = match line.trim().parse() {Ok(num) => num, Err(_) => break,};
+
+        if next > previous {
+            result += 1;
+        }
+        previous = next;
+    }
+
+    println!("result: {}", result);
 }
