e5b1a513d173b54aaedf9539196219bf9631f679
Mihailo Vidakovic <mihailo.system@gmail.com>
2021-12-08 18:59:42 +0100

    [sonar_sweep] Added solution for part 2
    Solved part 2 of the question
    Moved from using Option Iter to saving input into a vector for easier manipulation

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,23 +1,28 @@
 use std::fs;
 
+const UNDEFINED_NUMBER: i32 = -1;
+
 fn main() {
     let mut result = 0;
 
     let file_contents = fs::read_to_string("input.txt")
         .expect("Something went wrong");
 
-    let lines = file_contents.lines();
+    let list : Vec<i32> = file_contents.lines().map(|x| x.trim().parse().expect("panik")).collect();
     let mut previous : i32 = -1;
 
-    for next_line in lines {
-        let line : String = next_line.to_string();
-        let next : i32 = match line.trim().parse() {Ok(num) => num, Err(_) => break,};
+    let mut i = 0;
+    let num_of_lines = list.len();
 
-        if previous > -1 && next > previous {
+    while i < num_of_lines - 2 {
+        let next = list[i] + list[i+1] + list[i+2];
+
+        if previous != UNDEFINED_NUMBER && next > previous {
             result += 1;
         }
 
         previous = next;
+        i += 1;
     }
 
     println!("result: {}", result);
