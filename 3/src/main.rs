f9f4811bade6f6d67e736aadb6041c243eb048f3
Mihailo Vidakovic <mihailo.system@gmail.com>
2021-12-09 20:18:15 +0100

    [binary_disagnostic] Added solution for part 2
    Major code restructure due to logic change in the task itself.

--- 3/src/main.rs
+++ 3/src/main.rs
@@ -5,40 +5,49 @@ fn main() {
         .expect("Something went wrong");
 
     let lines : Vec<&str> = file_contents.lines().collect();
-    let lines_len = lines.len() as u32;
-    let mut counter_vector : Vec<u32> = Vec::new();
-    init_vector_with_num(&mut counter_vector, 0, lines[0].chars().count() as u32);
 
-    for line in lines {
-        for i in 0..line.chars().count() {
-            let character : char = line.chars().nth(i).unwrap();
-            if character.to_digit(10) == Some(1) {
-                counter_vector[i] += 1;
+    let mut generator_vec : Vec<&str> = lines;
+    let mut scrubber_vec : Vec<&str> = generator_vec.clone();
+
+    {
+        let mut generator_index = 0;
+        
+        while generator_vec.len() > 1 {
+            let mut one_counter = 0;
+            for item in generator_vec.clone() {
+                let character : char = item.chars().nth(generator_index).unwrap();
+                if character.to_digit(10) == Some(1) {
+                    one_counter += 1;
+                }
             }
+            let digit = if one_counter >= generator_vec.len() - one_counter {'1'} else {'0'};
+            
+            generator_vec.retain(|&s| s.chars().nth(generator_index).unwrap() == digit);
+            generator_index += 1;
         }
+
     }
 
-    let mut gamma = 0;
-    let mut epsilon = 0;
-    let base : i32 = 2;
-    let x = counter_vector.len() as u32;
-    for index in 0..x {
-        let item = counter_vector[index as usize];
-        let power = x - index - 1;
-        if item >= lines_len/2 {
-            gamma += base.pow(power);
-        }
-        else {
-            epsilon += base.pow(power);
-        }
-    }
+    {
+        let mut scrubber_index = 0;
+        
+        while scrubber_vec.len() > 1 {
+            let mut one_counter = 0;
+            for item in scrubber_vec.clone() {
+                let character : char = item.chars().nth(scrubber_index).unwrap();
+                if character.to_digit(10) == Some(1) {
+                    one_counter += 1;
+                }
+            }
+            let digit = if one_counter >= scrubber_vec.len() - one_counter {'0'} else {'1'};
 
-    println!("gamma : {} epsilon : {}", gamma, epsilon);
-    println!("result : {}", gamma * epsilon);
-}
+            scrubber_vec.retain(|&s| s.chars().nth(scrubber_index).unwrap() == digit);
+            scrubber_index += 1;
+        }
 
-fn init_vector_with_num(vector: &mut Vec<u32>, num: u32, size: u32) {
-    for _ in 0..size {
-        vector.push(num);
     }
+    let generator_rating = isize::from_str_radix(generator_vec[0], 2).unwrap();
+    let scrubber_rating = isize::from_str_radix(scrubber_vec[0], 2).unwrap();
+    println!("generator: {} scrubber: {}", generator_rating, scrubber_rating);
+    println!("solution: {}", generator_rating * scrubber_rating);
 }
