79654632423b45336f7bf950eaa02b2024970e78
Mihailo Vidakovic <mihailo.system@gmail.com>
2021-12-09 17:13:23 +0100

    [dive] Added solution for part 2

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -6,14 +6,15 @@ fn main() {
     
     let mut forward : i32 = 0;
     let mut horizontal : i32 = 0;
+    let mut aim : i32 = 0;
 
     let lines = file_contents.lines();
     for line in lines {
         let result : Vec<&str> = line.split(' ').collect();
-        match (result[0], &mut forward, &mut horizontal) {
-            ("forward", _, _) => { let addition : i32 = result[1].trim().parse().expect("Not a number"); forward += addition } ,
-            ("down", _, _) => { let addition : i32 = result[1].trim().parse().expect("Not a number"); horizontal += addition } ,
-            ("up", _, _) => { let addition : i32 = result[1].trim().parse().expect("Not a number"); horizontal -= addition } ,
+        match (result[0], &mut forward, &mut horizontal, &mut aim) {
+            ("forward", _, _, _) => { let addition : i32 = result[1].trim().parse().expect("Not a number"); forward += addition; horizontal += aim * addition; } ,
+            ("down", _, _, _) => { let addition : i32 = result[1].trim().parse().expect("Not a number"); aim += addition } ,
+            ("up", _, _, _) => { let addition : i32 = result[1].trim().parse().expect("Not a number"); aim -= addition } ,
             _ => println!("Incorrect input")
         }
     }
