db4fcd332dee17bec8d5ce5dc8a4827f3c8b6ebb
Mihailo Vidakovic <mihailo.system@gmail.com>
2021-12-16 15:18:16 +0100

    Stolen solution for part 2 that i still don't understand

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-const SIMULATION_DURATION : u8 = 80;
+const SIMULATION_DURATION : u64 = 18;
 
 fn main() {
     let file_contents = fs::read_to_string("input.txt")
@@ -13,7 +13,24 @@ fn main() {
 }
 
 fn solve_part1(lines : Vec<&str>) {
+    let fishes: Vec<usize> = lines[0]
+        .split(",")
+        .map(|i| i.parse().unwrap())
+        .collect();
+    let max_day:usize = 9;
+    let mut counter = vec![0;max_day];
+    for fish in fishes {
+        counter[fish]+=1;
+    }
+    let days:usize = 256;
+    for d in 0..days {
+        counter[(7+d)%max_day] += counter[d%max_day];
+    }
+    println!("{:?}", counter.iter().sum::<usize>());
 }
 
 fn solve_part2(lines : Vec<&str>) {
+    let mut result = 0;
+    
+    println!("part 2: {}", result);
 }
